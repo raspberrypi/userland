77437517d4526991b1fa318145b9e1f42abbf4fb
JamesH <jnahughes@googlemail.com>
2013-09-26 14:41:58 +0100

    userland #64
    Added ability to set the H264 profile for encoding.
    
    Supports main, baseline and high.
    
    Also needed to shuffle some code around to make some parsing
    code available outside raspicamcontrol.c

--- host_applications/linux/apps/raspicam/RaspiCLI.h
+++ host_applications/linux/apps/raspicam/RaspiCLI.h
@@ -38,9 +38,19 @@ typedef struct
    int num_parameters;
 } COMMAND_LIST;
 
+/// Cross reference structure, mode string against mode id
+typedef struct xref_t
+{
+   char *mode;
+   int mmal_mode;
+} XREF_T;
+
 
 void raspicli_display_help(const COMMAND_LIST *commands, const int num_commands);
 int raspicli_get_command_id(const COMMAND_LIST *commands, const int num_commands, const char *arg, int *num_parameters);
+
+int raspicli_map_xref(const char *str, const XREF_T *map, int num_refs);
+const char *raspicli_unmap_xref(const int en, XREF_T *map, int num_refs);
 
 
 #endif
