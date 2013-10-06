fe5d37a45489140c7c878e15936f0bdd8838975e
Karl Lew <karl@firepick.org>
2013-10-05 20:27:47 -0700

    jndi

---
+++ jndi/HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld
+{
+	private native void print();
+
+	public static void main(String[] args) {
+			new HelloWorld().print();
+	}
+
+	static {
+		System.loadLibrary("HelloWorld");
+	}
+}
