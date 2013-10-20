9a303bcd360092d27eccf0ec432286e200aa5eb3
Karl Lew <karl@firepick.org>
2013-10-20 08:51:41 -0700

    FireFuse

--- FireFuse.c
+++ FireFuse.c
@@ -17,8 +17,10 @@
 #include <errno.h>
 #include <fcntl.h>
 
-static const char *firefuse_str = "FireFuse Hello!\n";
-static const char *firefuse_path = "/picam";
+static const char *status_str = "FireFuse OK!\n";
+static const char *status_path = "/status";
+static const char *pnpcam_str = "FirePiCam picture\n";
+static const char *pnpcam_path = "/pnpcam.jpg";
 
 static int firefuse_getattr(const char *path, struct stat *stbuf)
 {
@@ -49,19 +51,31 @@ static int firefuse_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 
 	filler(buf, ".", NULL, 0);
 	filler(buf, "..", NULL, 0);
-	filler(buf, firefuse_path + 1, NULL, 0);
+	filler(buf, status_path + 1, NULL, 0);
+	filler(buf, pnpcam_path + 1, NULL, 0);
 
 	return 0;
 }
 
 static int firefuse_open(const char *path, struct fuse_file_info *fi)
 {
-	if (strcmp(path, firefuse_path) != 0)
+	if (strcmp(path, status_path) == 0) {
+		if ((fi->flags & 3) != O_RDONLY) {
+			return -EACCES;
+		}
+	} else if (strcmp(path, pnpcamp_path) == 0) {
+		if ((fi->flags & 3) != O_RDONLY) {
+			return -EACCES;
+		}
+	} else {
 		return -ENOENT;
+	}
 
-	if ((fi->flags & 3) != O_RDONLY)
-		return -EACCES;
+	return 0;
+}
 
+static int firefuse_close(const char *path, struct fuse_file_info *fi)
+{
 	return 0;
 }
 
@@ -70,16 +84,27 @@ static int firefuse_read(const char *path, char *buf, size_t size, off_t offset,
 {
 	size_t len;
 	(void) fi;
-	if(strcmp(path, firefuse_path) != 0)
+	if (strcmp(path, status_path) == 0) {
+		len = strlen(status_str);
+		if (offset < len) {
+			if (offset + size > len)
+				size = len - offset;
+			memcpy(buf, status_str + offset, size);
+		} else {
+			size = 0;
+		}
+	} else if (strcmp(path, pnpcam_path) == 0) {
+		len = strlen(pnpcam_str);
+		if (offset < len) {
+			if (offset + size > len)
+				size = len - offset;
+			memcpy(buf, pnpcam_str + offset, size);
+		} else {
+			size = 0;
+		}
+	} else {
 		return -ENOENT;
-
-	len = strlen(firefuse_str);
-	if (offset < len) {
-		if (offset + size > len)
-			size = len - offset;
-		memcpy(buf, firefuse_str + offset, size);
-	} else
-		size = 0;
+	}
 
 	return size;
 }
@@ -88,6 +113,7 @@ static struct fuse_operations firefuse_oper = {
 	.getattr	= firefuse_getattr,
 	.readdir	= firefuse_readdir,
 	.open		= firefuse_open,
+	.close		= firefuse_close,
 	.read		= firefuse_read,
 };
 
