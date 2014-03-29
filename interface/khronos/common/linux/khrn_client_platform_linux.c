f5eec2ea142c80e5d0ed16b62574a551730bb67d
Nick Hudson <skrll@netbsd.org>
2014-03-29 07:28:31 +0000

    Appease gcc 4.8 'may be used uninitialized in this function' warnings

--- interface/khronos/common/linux/khrn_client_platform_linux.c
+++ interface/khronos/common/linux/khrn_client_platform_linux.c
@@ -753,7 +753,7 @@ static EGL_DISPMANX_WINDOW_T *check_default(EGLNativeWindowType win)
          VC_RECT_T dst_rect;
          VC_RECT_T src_rect;
 
-         int x, y, width, height, layer;
+         int x = 0, y = 0, width = 0, height = 0, layer;
 
          switch(wid)
          {
