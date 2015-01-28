3b81b91c18ff19f97033e146a9f3262ca631f0e9
popcornmix <popcornmix@gmail.com>
2015-01-28 18:43:21 +0000

    hello_fft: Use addresses from device tree rather than hard-coded constants

--- host_applications/linux/libs/bcm_host/bcm_host.c
+++ host_applications/linux/libs/bcm_host/bcm_host.c
@@ -138,4 +138,36 @@ void wfc_stream_await_buffer(void * stream)
    vcos_assert(0);
 }
 
+static unsigned get_dt_ranges(const char *filename, unsigned offset)
+{
+   unsigned address = ~0;
+   FILE *fp = fopen(filename, "rb");
+   if (fp)
+   {
+      unsigned char buf[4];
+      fseek(fp, offset, SEEK_SET);
+      if (fread(buf, 1, sizeof buf, fp) == sizeof buf)
+         address = buf[0] << 24 | buf[1] << 16 | buf[2] << 8 | buf[3] << 0;
+      fclose(fp);
+   }
+   return address;
+}
+
+unsigned bcm_host_get_peripheral_address(void)
+{
+   unsigned address = get_dt_ranges("/proc/device-tree/soc/ranges", 4);
+   return address == ~0 ? 0x20000000 : address;
+}
+
+unsigned bcm_host_get_peripheral_size(void)
+{
+   unsigned address = get_dt_ranges("/proc/device-tree/soc/ranges", 8);
+   return address == ~0 ? 0x01000000 : address;
+}
+
+unsigned bcm_host_get_sdram_address(void)
+{
+   unsigned address = get_dt_ranges("/proc/device-tree/axi/vc_mem/reg", 8);
+   return address == ~0 ? 0x40000000 : address;
+}
 
