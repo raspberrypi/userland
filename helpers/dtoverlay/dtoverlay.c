d9a493ba3cc2da8a01c79b18b77f4c69cb0ba53e
Phil Elwell <phil@raspberrypi.org>
2017-11-29 12:58:01 +0000

    dtoverlay: Create "/aliases" node when needed
    Attempting to add an alias currently fails if there is no
    existing "/aliases" node. Prevent an error by creating one
    as necessary.
    
    See: https://github.com/raspberrypi/firmware/issues/906
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.org>

--- helpers/dtoverlay/dtoverlay.c
+++ helpers/dtoverlay/dtoverlay.c
@@ -1948,6 +1948,8 @@ int dtoverlay_set_alias(DTBLOB_T *dtb, const char *alias_name, const char *value
    int node_off;
 
    node_off = fdt_path_offset(dtb->fdt, "/aliases");
+   if (node_off < 0)
+       node_off = fdt_add_subnode(dtb->fdt, 0, "aliases");
 
    return fdt_setprop_string(dtb->fdt, node_off, alias_name, value);
 }
