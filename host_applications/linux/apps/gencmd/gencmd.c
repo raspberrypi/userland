517cdc30da167d81a485e7a994e02cec2390a269
andrum99 <49293562+andrum99@users.noreply.github.com>
2019-05-01 13:50:52 +0100

    Improve vcgencmd (#552)
    Add basic usage, detect and report errors to STDERR, and add link to forthcoming documentation on the website.

--- host_applications/linux/apps/gencmd/gencmd.c
+++ host_applications/linux/apps/gencmd/gencmd.c
@@ -36,41 +36,60 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "interface/vmcs_host/vc_vchi_gencmd.h"
 
+void show_usage()
+{
+   puts( "Usage: vcgencmd [-t] command" );
+   puts( "Send a command to the VideoCore and print the result.\n" );
+   puts( "  -t          Time how long the command takes to complete" );
+   puts( "  -h, --help  Show this information\n" );
+   puts( "Use the command 'vcgencmd commands' to get a list of available commands\n" );
+   puts( "Exit status:" );
+   puts( "   0    command completed successfully" );
+   puts( "  -1    problem with VCHI" );
+   puts( "  -2    VideoCore returned an error\n" );
+   puts( "For further documentation please see" );
+   puts( "https://www.raspberrypi.org/documentation/raspbian/applications/vcgencmd.md\n" );
+}
+
 int main( int argc, char **argv )
 {
-   int instNum = 0;
    VCHI_INSTANCE_T vchi_instance;
    VCHI_CONNECTION_T *vchi_connection = NULL;
 
-   if ( argc > 1 )
+   if ( argc == 1 )
    {
-       if (( strcmp( argv[1], "0" ) == 0 ) || ( strcmp( argv[1], "1" ) == 0 ))
-       {
-           instNum = atoi( argv[1] );
-           argv++;
-           argc--;
-       }
+      // no arguments passed, so show basic usage
+      show_usage();
+      return 0;
    }
 
    vcos_init();
 
-    if ( vchi_initialise( &vchi_instance ) != 0)
-    {
-        printf( "VCHI initialization failed\n" );
-        return -1;
-    }
+   if ( vchi_initialise( &vchi_instance ) != 0)
+   {
+      fprintf( stderr, "VCHI initialization failed\n" );
+      return -1;
+   }
 
-    //create a vchi connection
-    if ( vchi_connect( NULL, 0, vchi_instance ) != 0)
-    {
-        printf( "VCHI connection failed\n" );
-        return -1;
-    }
+   //create a vchi connection
+   if ( vchi_connect( NULL, 0, vchi_instance ) != 0)
+   {
+      fprintf( stderr, "VCHI connection failed\n" );
+      return -1;
+   }
 
-    vc_vchi_gencmd_init(vchi_instance, &vchi_connection, 1 );
+   vc_vchi_gencmd_init(vchi_instance, &vchi_connection, 1 );
 
-    if (argc > 1)
-    {
+   if (argc > 1)
+   {
+      // first check if we were invoked with either -h or --help
+      // in which case show basic usage and exit
+      if( strcmp( argv[1], "-h" ) == 0 || strcmp( argv[1], "--help" ) == 0 )
+      {
+         show_usage();
+         return 0;
+      }
+
       int i = 1;
       char buffer[ 1024 ];
       size_t buffer_offset = 0;
@@ -101,47 +120,54 @@ int main( int argc, char **argv )
       //send the gencmd for the argument
       if (( ret = vc_gencmd_send( "%s", buffer )) != 0 )
       {
-          printf( "vc_gencmd_send returned %d\n", ret );
+         printf( "vc_gencmd_send returned %d\n", ret );
       }
 
       //get + print out the response!
       if (( ret = vc_gencmd_read_response( buffer, sizeof( buffer ) )) != 0 )
       {
-          printf( "vc_gencmd_read_response returned %d\n", ret );
+         printf( "vc_gencmd_read_response returned %d\n", ret );
       }
 
       if( show_time )
-         after = clock();
-
-      if( show_time )
       {
+         after = clock();
          time_diff = ((double) (after - before)) / CLOCKS_PER_SEC;
-
-         printf( "Time took %f seconds (%f msecs) (%f usecs)\n", time_diff, time_diff * 1000, time_diff * 1000000 );
+         printf( "Time taken: %f seconds (%f msecs) (%f usecs)\n", time_diff, time_diff * 1000, time_diff * 1000000 );
       }
 
       if ( buffer[0] != '\0' )
       {
-          if ( buffer[ strlen( buffer) - 1] == '\n' )
-          {
-              fputs( buffer, stdout );
-          }
-          else
-          {
-              printf("%s\n", buffer );
-          }
+         if ( buffer[ strlen( buffer) - 1] == '\n' )
+         {
+            fputs( buffer, stdout );
+         }
+         else
+         {
+            if (strncmp( buffer, "error=", 6) == 0 )
+            {
+               fprintf (stderr, "%s\n", buffer);
+               if ( strcmp( buffer, "error=1 error_msg=\"Command not registered\"" ) == 0 )
+               {
+                  fprintf( stderr, "Use 'vcgencmd commands' to get a list of commands\n" );
+               }
+               return -2;
+            }
+            else
+            {
+               puts(buffer);
+            }
+         }
       }
-    }
+   }
 
-    vc_gencmd_stop();
+   vc_gencmd_stop();
 
-    //close the vchi connection
-    if ( vchi_disconnect( vchi_instance ) != 0)
-    {
-        printf( "VCHI disconnect failed\n" );
-        return -1;
-    }
-
+   //close the vchi connection
+   if ( vchi_disconnect( vchi_instance ) != 0)
+   {
+      fprintf( stderr, "VCHI disconnect failed\n" );
+      return -1;
+   }
    return 0;
 }
-
